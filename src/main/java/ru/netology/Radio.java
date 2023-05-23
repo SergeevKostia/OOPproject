823108ae3b715a68bac356e9ba7451460be55522
Konstantin Sergeev <sergeeff.kossta@gmail.com>
2023-05-23 14:44:58 +0300

    change lombok

--- src/main/java/ru/netology/Radio.java
+++ src/main/java/ru/netology/Radio.java
@@ -1,5 +1,12 @@
 package ru.netology;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class Radio {
     public int maxWave = 9;
     public int minWave = 0;
@@ -7,69 +14,7 @@ public class Radio {
     public int minVolume = 0;
     public int currentRadioWave = minWave;
     public int currentVolume = minVolume;
-
-    public int getCurrentRadioWave() {
-
-        return currentRadioWave;
-    }
-
-    public int getCurrentVolume() {
-
-        return currentVolume;
-    }
-
-    public void setCurrentRadioWave(int currentRadioWave) {
-        if (currentRadioWave < minWave) {
-            return;
-        }
-        if (currentRadioWave > maxWave) {
-            return;
-        }
-
-        this.currentRadioWave = currentRadioWave;
-    }
-
-    public void nextRadioWave() {
-
-        if (currentRadioWave != maxWave) {
-            currentRadioWave++;
-        } else {
-            currentRadioWave = minWave;
-        }
-
-
-    }
-
-
-    public void stepBackRadioWave() {
-
-        if (currentRadioWave != minWave) {
-            currentRadioWave--;
-        } else {
-            currentRadioWave = maxWave;
-        }
-    }
-
-    public void setCurrentVolume(int newCurrentVolume) {
-        if (newCurrentVolume < minVolume) {
-            return;
-        }
-        if (newCurrentVolume > maxVolume) {
-            return;
-        }
-        currentVolume = newCurrentVolume;
-
-    }
-
-    public void increaseVolume() {
-        if (currentVolume < maxVolume) {
-            currentVolume++;
-        }
-    }
-
-    public void reduceVolume() {
-        if (currentVolume > minVolume) {
-            currentVolume--;
-        }
-    }
 }
+
+
+
